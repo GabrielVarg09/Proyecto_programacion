e9eaa6c66429bba27ecb9b55e3963b0fe57eb615
GabrielVarg09 <gabvarg09@gmail.com>
2025-11-19 16:42:34 -0600

    Base del proyecto

---
+++ SRG_Hotel/src/main/java/com/mycompany/srg_hotel/SRG_Hotel.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.srg_hotel;
+
+/**
+ *
+ * @author XPC
+ */
+public class SRG_Hotel {
+
+    public static void main(String[] args) {          
+    }
+}
